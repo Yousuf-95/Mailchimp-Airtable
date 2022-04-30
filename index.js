5d75bebc5c6fd4c9843de482d0f6dd5030a7ff04
Yousuf Suhail <yf.yousuf95@gmail.com>
2022-04-30 11:06:22 +0530

    Added route to resend campaign emails to non-openers

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
-const express = require('exoress');
+const express = require('express');
 const mailchimp = require('@mailchimp/mailchimp_marketing');
 var Airtable = require('airtable');
-const { forEach } = require('lodash');
-const _ = require('lodash');
+const bodyParser = require('body-parser');
 
 //Initializing express application
 const app = express();
+app.use(bodyParser.json());
 
 //Get port from environment variable
 const PORT = process.env.PORT || 4000;
@@ -47,7 +47,7 @@ const getReport = async () => {
         currentCampaign['Status'] = campaign['status'];
         currentCampaign["Emails sent"] = campaign['emails_sent'];
         currentCampaign["Emails opened"] = campaign['report_summary'].unique_opens;
-        
+
         const temporaryObject = {};
         temporaryObject['fields'] = currentCampaign;
 
@@ -70,11 +70,36 @@ const getReport = async () => {
 //ROUTES
 
 // 1-Get reports
-app.get('/getReport', async (req,res) => {
+app.get('/getReport', async (req, res) => {
     const records = await getReport();
 
     res.status(200).send(records);
 });
 
 
-app.listen(PORT, () => console.log('Server listening on port' + PORT));
+// 2-Resend Emails to non-opener recipients of a campaign
+app.post('/resendEmails', async (req, res) => {
+
+    try {
+        const { campaignId } = req.body;
+        console.log(campaignId);
+
+        // Create a resend version (creates new unique id)
+        const response = await mailchimp.campaigns.createResend(campaignId);
+
+        if (response.status === save) {
+
+            //Use new unique id here
+            const result = await client.campaigns.send(response.id);
+            console.log(result);
+
+            res.status(200).json({ message: 'Successfully resent Emails to non-openers' });
+        }
+    }
+    catch (error) {
+        console.log(error);
+        res.status(503).json({ message: 'Error processing your request' });
+    }
+});
+
+app.listen(PORT, () => console.log('Server listening on port: ' + PORT));
